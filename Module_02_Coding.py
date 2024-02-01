46002e261518d22c5aa3765db98e713035491134
grubbyinfamous <jpbucu2002@gmail.com>
2024-02-01 12:57:44 -0600

    updated

--- Module_02_Coding.py
+++ Module_02_Coding.py
@@ -15,19 +15,23 @@ Display the appooximate age of the user
 '''
 
 #Here is where I ask for the user's name
-user_Name = input("Hello! What is your name?")
-print("It is nice to meet you", user_Name)
+#user_Name = input("Hello! What is your name?")
+#print("It is nice to meet you", user_Name)
 
 #Here is where I ask for what the current year is
-current_Year = int(input("Would you kindly tell me what the current year is? "))
-print("The current year, according to you is:", current_Year)
+#current_Year = int(input("Would you kindly tell me what the current year is? "))
+#print("The current year, according to you is:", current_Year)
 
 #Here is where I ask for the user's birth year
-user_BY = int(input("Would you kindly tell me what year you were born?"))
-print(user_Name, ", you were born in the year", user_BY)
+#user_BY = int(input("Would you kindly tell me what year you were born?"))
+#print(user_Name, ", you were born in the year", user_BY)
 
 #Here is where I calculate the user's approximate age
-user_Age = current_Year - user_BY
+#user_Age = current_Year - user_BY
 
 #Here is where I will display the user's current age
-print("Hello",user_Name,". According to your birth year, you are approximately", user_Age, "years old")
+#print("Hello",user_Name,". According to your birth year, you are approximately", user_Age, "years old")
+val = 123456.789
+print(f'{val:,.2f}')
+print(f'{0.2:.0%}')
+print('One''Two''Three')
