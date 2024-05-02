cc94f0615f77a766f25f5354f8153557d2ae3e50
grubbyinfamous <jpbucu2002@gmail.com>
2024-05-01 23:19:25 -0500

    Challenge 14

---
+++ Challenge_14.py
@@ -0,0 +1,15 @@
+'''
+Things to do:
+Make a star on the hollywood walk of fame
+Make a star similar to the one page 775 of the textbook
+Bonus: Make a button that changes the color of the star to solid black and the text with my name to white
+'''
+
+from tkinter import *
+
+root = Tk()
+root.geometry(1000, 1000)
+def create_Star():
+    canvas = Canvas(root)
+    
+    
